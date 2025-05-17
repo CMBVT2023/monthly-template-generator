79fcb1ae2d77b1427698a59339d6ec602dc5a0b5
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2025-05-16 20:29:51 -0700

    Initailzied the date range picker

--- src/containers/main-page.tsx
+++ src/containers/main-page.tsx
@@ -1,29 +1,92 @@
-'use client'
+"use client";
 
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
 import { Input } from "@/components/ui/input";
-import { useState } from "react"
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import { Switch } from "@/components/ui/switch";
+import { addDays, format } from "date-fns";
+import { CalendarIcon } from "lucide-react";
+import { useState } from "react";
+import { DateRange } from "react-day-picker";
 
 export default function MainPage() {
-    const [ xCoordinate, setXCoordinate ] = useState<number>(0);
-    const [ yCoordinate, setYCoordinate ] = useState<number>(0);
+  const [xCoordinate, setXCoordinate] = useState<number>(0);
+  const [yCoordinate, setYCoordinate] = useState<number>(0);
 
-    const [ templateFile, setTemplateFile ] = useState<File | null>(null);
+  const [templateFile, setTemplateFile] = useState<File | null>(null);
 
-    
+  const [dateRange, setDateRange] = useState<DateRange | undefined>({
+    from: new Date(),
+    to: addDays(new Date(), 30),
+  });
 
-    const [ isExcluding, setIsExcluding ] = useState<boolean>(false);
-    
-    const [ selectedDaysOfWeek, setSelectedDaysOfWeek ] = useState<string[]>([]);
+  const [isExcluding, setIsExcluding] = useState<boolean>(false);
 
-    return (
-        <div>
-            <Input value={xCoordinate} onChange={(e) => setXCoordinate(parseInt(e.target.value))} type="number" min={0} max={100} />
-            <Input value={yCoordinate} onChange={(e) => setYCoordinate(parseInt(e.target.value))} type="number" min={0} max={100} />
+  const [selectedDaysOfWeek, setSelectedDaysOfWeek] = useState<string[]>([]);
 
-            <Input onChange={(e) => setTemplateFile(e.target.files[0])} type="file" accept="application/pdf"/>
+  return (
+    <div>
+      <Input
+        value={xCoordinate}
+        onChange={(e) => setXCoordinate(parseInt(e.target.value))}
+        type="number"
+        min={0}
+        max={100}
+      />
+      <Input
+        value={yCoordinate}
+        onChange={(e) => setYCoordinate(parseInt(e.target.value))}
+        type="number"
+        min={0}
+        max={100}
+      />
 
-            
+      <Input
+        onChange={(e) => setTemplateFile(e.target.files[0])}
+        type="file"
+        accept="application/pdf"
+      />
 
-        </div>
-    )
+      <Switch
+        checked={isExcluding}
+        onCheckedChange={() => setIsExcluding(!isExcluding)}
+      />
+
+      
+
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button
+            id="date"
+            variant={"outline"}
+          >
+            <CalendarIcon />
+            {dateRange?.from ? (
+              dateRange.to ? (
+                <>
+                  {format(dateRange.from, "LLL dd, y")} -{" "}
+                  {format(dateRange.to, "LLL dd, y")}
+                </>
+              ) : (
+                format(dateRange.from, "LLL dd, y")
+              )
+            ) : (
+              <span>Pick a date</span>
+            )}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-auto p-0" align="start">
+          <Calendar
+            initialFocus
+            mode="range"
+            defaultMonth={dateRange?.from}
+            selected={dateRange}
+            onSelect={setDateRange}
+            numberOfMonths={2}
+          />
+        </PopoverContent>
+      </Popover>
+    </div>
+  );
 }
