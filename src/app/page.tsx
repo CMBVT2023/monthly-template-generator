09dd5a5f4306822420d0f75ea2e41605c49c0a04
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2025-05-16 20:10:02 -0700

    Added input component from shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,10 @@
-import { Slider } from "@/components/ui/slider";
 
 export default function Home() {
   return (
     <div>
-      <h1>Select a file:</h1>
+      
 
-      <Slider 
-        defaultValue={[50]}
-        max={100}
-        step={1}
-      />
+      
     </div>
   );
 }
